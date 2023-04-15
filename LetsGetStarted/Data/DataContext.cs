e616966cb62663a7d481e2bf244ddb9e6b28fbfa
UddeshyaVerma <vuddeshya11@gmail.com>
2023-04-15 15:42:23 +0530

    Add project files.

---
+++ LetsGetStarted/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using LetsGetStarted.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LetsGetStarted.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
